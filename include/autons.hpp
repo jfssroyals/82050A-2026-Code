063ca0b519d9acaa3d42f99f8238c04257d4b0eb
Shanvir Sidhu <shanvirsidhu123@gmail.com>
2026-06-28 20:31:12 -0400

    basic claw functionality

---
+++ include/autons.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "main.h"
+#include "lemlib/api.hpp"
+
+// global objects defined in main.cpp
+extern pros::Controller controller;
+extern lemlib::Chassis chassis;
+
+// auton routines
---
+++ src/claw.cpp
@@ -0,0 +1,3 @@
+#include "main.h"
+#include "claw.hpp"
+#include "api.h"
