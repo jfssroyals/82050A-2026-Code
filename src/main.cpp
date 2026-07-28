b595c5310e4d2e9673150c1b777163afb6e32c1e
Vikas Sethuraman <vikassethuraman@gmail.com>
2026-07-28 17:46:11 -0400

    automatically comes back after claw

--- src/main.cpp
+++ src/main.cpp
@@ -206,10 +206,13 @@ void opcontrol() {
         if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) { 
             claw.toggle();
             pros::delay(450);
+            leftMotors.move(25);  // Power range: -127 to 127
+            rightMotors.move(25);
+            pros::delay(10);
             // pros::lcd::print(5,  "Boolean: %.2f", claw.isopen());
             if (claw.isopen() == true){
                 bar.motor.move(-60);
-                pros::delay(400);
+                pros::delay(200);
                 bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
                 bar.motor.brake();
             }
