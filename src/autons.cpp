a96fee1b4e2ac220096333fb7bfde4a64351232c
Vishnu <vishnu.radhajay@gmail.com>
2026-07-30 13:19:51 -0400

    Lead=0 when move pose

--- src/autons.cpp
+++ src/autons.cpp
@@ -14,56 +14,54 @@ extern Claw claw;
 void fourPinBlue() {
     pros::delay(2000);
     
-    // Seamless transition into odometry tracking
-    chassis.setPose(0, 0, 0);
-    // chassis.setPose(0, 0, 350);
+    chassis.setPose(0, 0, 350);
 
-    // // Open-loop movements (continuous motion)
-    // chassis.arcade(127, 127);
-    // pros::delay(700);
-    // chassis.arcade(-80, -127);
-    // pros::delay(200);
-    // chassis.arcade(127, 127);
-    // pros::delay(500);
-    // chassis.arcade(-100, 0);
-    // pros::delay(300);
+    // Open-loop movements (continuous motion)
+    chassis.arcade(127, 127);
+    pros::delay(700);
+    chassis.arcade(-80, -127);
+    pros::delay(200);
+    chassis.arcade(127, 127);
+    pros::delay(500);
+    chassis.arcade(-100, 0);
+    pros::delay(300);
 
-    // chassis.swingToHeading(270, DriveSide::RIGHT, 2000,{
-    //     .minSpeed = 127,
-    //     .earlyExitRange = 20
-    // });
+    chassis.swingToHeading(270, DriveSide::RIGHT, 2000,{
+        .minSpeed = 127,
+        .earlyExitRange = 20
+    });
 
-    // // Subsystem motion while moving
-    // bar.motor.move(60);
-    // pros::delay(200);
-    // bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-    // bar.motor.brake();
+    // Subsystem motion while moving
+    bar.motor.move(60);
+    pros::delay(200);
+    bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    bar.motor.brake();
 
-    // chassis.waitUntilDone();
+    chassis.waitUntilDone();
 
-    // chassis.arcade(-127, 0);
-    // bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    // pros::delay(500);
-    // chassis.arcade(0, 0);
-    // claw.open();
-    // pros::delay(100);
+    chassis.arcade(-127, 0);
+    bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    pros::delay(500);
+    chassis.arcade(0, 0);
+    claw.open();
+    pros::delay(100);
 
-    // // next movement to pickup first pin cup stack
-    // chassis.moveToPose(3, -5, 270, 800);
-    // chassis.waitUntilDone();
-    // // Turn
-    // chassis.turnToHeading(30, 800);
+    // next movement to pickup first pin cup stack
+    chassis.moveToPose(3, -5, 270, 800);
+    chassis.waitUntilDone();
+    // Turn
+    chassis.turnToHeading(30, 800);
 
-    // // Start bar movement in the background 
-    // pros::Task barTask([]{
-    //     bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    //     bar.motor.move(127);
-    //     pros::delay(800);
-    //     bar.motor.brake();
-    // });
+    // Start bar movement in the background 
+    pros::Task barTask([]{
+        bar.motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+        bar.motor.move(127);
+        pros::delay(800);
+        bar.motor.brake();
+    });
 
     //
-    chassis.moveToPoint(0, 30, 2000);
+    chassis.moveToPose(0, 30, 0, 2000, {.lead = 0});
     chassis.waitUntilDone();
 }
 
